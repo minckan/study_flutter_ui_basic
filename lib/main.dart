df96be1932ab530b0793346cfe79280bd51579ae
mz-minckan <mymy317@naver.com>
2023-03-31 18:33:15 +0900

    add Webtoon app logic at this project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screen/home_screen.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -11,40 +11,21 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
 
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
 
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
-    );
-  }
-}
+//  theme: ThemeData(
+//   colorScheme: ColorScheme.fromSwatch(
+//     backgroundColor: const Color(0xFFE7626C),
+//   ),
+//   textTheme: const TextTheme(
+//     displayLarge: TextStyle(
+//       color: Color(0xFF232B55),
+//     ), 
+//   ),
+//   cardColor: const Color(0xFFF4EDDB),
+// ),
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
+import 'package:toonflix/widgets/webtoon_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 22,
+          ),
+        ),
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, future) {
+          if (future.hasData) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(future),
+                )
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> future) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: future.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        var webtoon = future.data![index];
+        return WebtoonWidget(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
+      ),
+    );
+  }
+}
